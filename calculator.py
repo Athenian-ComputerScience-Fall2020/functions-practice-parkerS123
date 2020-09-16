c069e748972d4c589ebd9015496fab37b8c7c6e7
parkerS123 <23pschwantes@athenian.org>
2020-09-16 22:08:06 +0000

    parameter for calculator

--- calculator.py
+++ calculator.py
@@ -3,16 +3,16 @@ Collaborators: Megan helped me
 
 '''
 
-def addition():
+def addition(x,y):
     return x + y
 
-def subtraction():
+def subtraction(x,y):
     return x - y
 
-def multiplication():
+def multiplication(x,y):
     return x * y
 
-def division():
+def division(x,y):
     return x / y 
 
 x = int(input("Please enter a number: "))
@@ -21,12 +21,12 @@ y = int(input("Please enter a number: "))
 opperation = int(input("If addition enter 1, if subtraction enter 2, if multiplication enter 3, if division enter 4: "))
 
 if opperation == 1:
-    print("The sum is " + str(addition()))
+    print("The sum is " + str(addition(x,y)))
 elif opperation == 2:
-    print("The difference is " + str(subtraction()))
+    print("The difference is " + str(subtraction(x,y)))
 elif opperation == 3:
-    print("The product is " + str(multiplication()))
+    print("The product is " + str(multiplication(x,y)))
 elif opperation == 4:
-    print("The quotient is " + str(division()))
+    print("The quotient is " + str(division(x,y)))
 else:
     print("You did not enter 1,2,3 or 4. Try again...")
